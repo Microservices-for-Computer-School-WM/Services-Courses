2df58078139ed2bb58058102b956debfc2f2e202
ultimastrike <ultimastrike@gmail.com>
2023-04-11 10:35:24 -0400

    Full code checkin from Swamy (#2)
    Co-authored-by: AISWillMiller <will.miller@ais.com>

---
+++ Source/Courses.API/Extensions/DependedServicesExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Courses.API.ApplicationCore.Interfaces;
+using Courses.API.Business;
+using Courses.API.Configuration;
+using Courses.API.Persistence;
+using Courses.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Courses.API.Extensions;
+
+public static class DependedServicesExtensions
+{
+
+    public static IServiceCollection ConfigureDependedServices(this IServiceCollection services)
+    {
+        _ = services.AddDbContext<SchoolDbContext>(options => options.UseInMemoryDatabase(InMemoryDatabase.Name));
+
+        _ = services.AddAutoMapper(typeof(AutoMapperConfig));
+
+        _ = services.AddScoped<ICoursesBusiness, CoursesBusiness>();
+
+        _ = services.AddScoped<ICoursesRepository, CoursesRepository>();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        _ = services.AddEndpointsApiExplorer();
+        _ = services.AddSwaggerGen();
+
+        _ = services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy => policy.AllowAnyHeader()
+                                                            .WithOrigins("http://localhost:4007")
+                                                            .AllowAnyMethod());
+        });
+
+        return services;
+    }
+}
